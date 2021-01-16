718ed918360e0e58aa0c96c7c5ca192a2e6e8b8f
AisenFei <1374873433@qq.com>
2021-01-16 23:46:42 +0800

    Queue

---
+++ 栈与队列/队列方法/QueueDemo.java
@@ -0,0 +1,17 @@
+package code_2021_0116;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class QueueDemo {
+    public static void main(String[] args) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(1);
+        queue.add(2);
+        queue.add(3);
+
+        System.out.println(queue.element());
+        System.out.println(queue.element());
+        System.out.println(queue.element());
+    }
+}
